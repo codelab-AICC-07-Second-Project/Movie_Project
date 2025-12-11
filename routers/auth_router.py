8a1f7bcaed5ff830f9dd53458afe502288814560
nightcord25 <ywy0208@gmail.com>
2025-12-11 10:51:51 +0900

    원영 commit

--- routers/auth_router.py
+++
@@ -1,3 +0,0 @@
-from fastapi import APIRouter
-
-from db.au_db import get_db_connection
--- routers/chart_router.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(
-    prefix="/chart",
-    tags=["chart"],
-)
